fe537d9d5bb708b04fcce54b1e74966cb51fb3fd
longmatthewh <longmatthewh@gmail.com>
2014-05-20 12:27:57 -0400

    added 2nd tree

--- js/a11yTree.init.js
+++ js/a11yTree.init.js
@@ -1,4 +1,5 @@
 $('#code-example-real').html($('#simple-list-example').html());
+$('#code-example-real').find(' > ul').attr('id', 'example-tree');
 $('#code-example-real').find('h3').attr('id', 'tree-label');
 $('#code-example-real > ul').a11yTree({
     treeLabelId :'tree-label',
@@ -12,6 +13,24 @@ $('#code-example-real > ul').a11yTree({
     },
     onExpand: function($item, event) {
         $item.children('.at-toggle').removeClass('fa-plus-square').addClass('fa-minus-square');
+        if (event) {
+            ga('send', 'event', 'Toggle', 'Expand');
+        }
+    }
+});
+
+$('#other-code-example-real > ul').a11yTree({
+    treeLabelId :'other-tree-label',
+    toggleSelector : 'i.fa',
+    treeItemLabelSelector : '.tree-item-label',
+    onCollapse: function($item, event) {
+        $item.children('.at-toggle').removeClass('fa-caret-square-o-up').addClass('fa-caret-square-o-down');
+        if (event) {
+            ga('send', 'event', 'Toggle', 'Collapse');
+        }
+    },
+    onExpand: function($item, event) {
+        $item.children('.at-toggle').removeClass('fa-caret-square-o-down').addClass('fa-caret-square-o-up');
         if (event) {
             ga('send', 'event', 'Toggle', 'Expand');
         }
